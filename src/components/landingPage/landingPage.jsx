11c8e1f1ed3fbaafc09e5d557a93334bc518a9d7
samBoyle <sboyle05@gmail.com>
2023-11-20 13:11:00 -0800

    initial

---
+++ src/components/landingPage/landingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './landingPage.css'
+
+const LandingPage = () => {
+
+return (
+  <>
+  <section className='landingPageContainer'>
+  <h1>Telswitch</h1>
+  </section>
+  </>
+)
+
+}
+
+export default LandingPage
---
+++ src/components/services/services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './services.css';
+
+const Services = () => {
+
+  return (
+    <section className='servicesContainer'>
+      <h1>Services</h1>
+    </section>
+  )
+}
+
+export default Services;
