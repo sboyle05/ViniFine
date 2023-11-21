2c30cb30dafd5f9d922960e2bb62f129dce12edf
samBoyle <sboyle05@gmail.com>
2023-11-20 16:37:06 -0800

    background in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './tailwind.css';
 import './App.css';
 import LandingPage from './components/landingPage/landingPage';
 import About from './components/about/about';
@@ -7,15 +8,18 @@ import Services from './components/services/services';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/services' element={<Services/>}/>
-      </Routes>
-    </Router>
+    <section className="custom-bg min-h-screen" style={{ backgroundImage: "url('https://d2e39dj8x3uchh.cloudfront.net/barrels1.jpg')" }}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/services' element={<Services/>}/>
+        </Routes>
+      </Router>
+    </section>
   );
 }
+
 
 export default App;
--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -1,16 +1,35 @@
+// Import React and Tailwind's CSS directly
 import React from 'react';
-import './landingPage.css'
+import '../../tailwind.css';
 
 const LandingPage = () => {
+	return (
+		<section className='min-h-screen bg-gray-100 flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8'>
+			<div className='flex flex-col justify-center items-center h-full'>
+        <div className='bg-black bg-opacity-50 text-white p-4 rounded-lg max-w-2xl'>
+          <h1 className='text-4xl font-bold mb-6 text-center'>ViniFine</h1>
+			<p className='text-md md:text-lg text-gray-700 max-w-prose mb-4'>
+				At ViniFine, we offer a comprehensive range of equipment rentals and
+				specialized services tailored to the winemaking industry. Our offerings
+				include innovative solutions such as Custom Filtration, VA (Volatile
+				Acidity) Reduction, Smoke Taint Removal, and pH Adjustment, specifically
+				designed for wineries. We stand out in the market for our commitment to
+				enhancing wine quality and production efficiency. Our state-of-the-art
+				equipment and expert consultation make us an essential resource for
+				wineries looking to improve their processes and product quality. Focused
+				on customer satisfaction and technological advancement, ViniFine is your
+				key partner in achieving excellence in the art of winemaking.
+			</p>
+			<p className='mb-4'>
+				For more information on how we can assist with your winemaking needs,
+				please visit our Services page. If you're ready to enhance your wine
+				production or have any questions, reach out to us on our Contact
+				page—we're here to help.
+			</p>
+      </div>
+      </div>
+		</section>
+	);
+};
 
-return (
-  <>
-  <section className='landingPageContainer'>
-  <h1>Telswitch</h1>
-  </section>
-  </>
-)
-
-}
-
-export default LandingPage
+export default LandingPage;
