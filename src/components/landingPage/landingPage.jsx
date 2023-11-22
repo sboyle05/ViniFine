0d30958c13e51cb5f74b7a1638dc6494d879a38a
samBoyle <sboyle05@gmail.com>
2023-11-21 19:07:54 -0800

    vid background

--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -4,35 +4,16 @@ import '../../tailwind.css';
 
 const LandingPage = () => {
 	return (
-		<section className='bg-blackish min-h-screen flex flex-row mt-0 px-4 sm:px-6 lg:px-8'>
-			<div
-				className='mt-4 flex-1 custom-bg rounded-lg'
-				style={{
-					height: '80vh',
-					backgroundImage:
-						"url('https://d2e39dj8x3uchh.cloudfront.net/barrels1.jpg')",
-					backgroundSize: 'cover',
-					backgroundPosition: 'center',
-				}}
-			/>
-			<div className='mt-4 flex-1 flex flex-col justify-center items-center h-full'>
-				<div className='bg-gold p-4 rounded-lg max-w-2xl ml-3'>
-					<h2 className='text-4xl text-redish font-bold mb-2 text-center'>
-						Professional Filtration Services
-					</h2>
-					<p className='mb-4 text-white'>
-						At ViniFine, we offer a comprehensive range of equipment rentals and
-						specialized services tailored to the winemaking industry. Our
-						offerings include innovative solutions such as Custom Filtration, VA
-						(Volatile Acidity) Reduction, Smoke Taint Removal, and pH
-						Adjustment, specifically designed for wineries. We stand out in the
-						market for our commitment to enhancing wine quality and production
-						efficiency. Our state-of-the-art equipment and expert consultation
-						make us an essential resource for wineries looking to improve their
-						processes and product quality. Focused on customer satisfaction and
-						technological advancement, ViniFine is your key partner in achieving
-						excellence in the art of winemaking.
-					</p>
+
+       <div className='mt-28 w-full h-full flex justify-center items-center px-4 sm:px-6 lg:px-8'>
+        <div className='bg-black bg-opacity-50 p-4 rounded-lg max-w-2xl'>
+        <h2 className='text-3xl text-white font-bold mb-2 text-center'>
+            GRAPES, BARRELS, WINE
+          </h2>
+          <h2 className='text-2xl text-white font-bold mb-2 text-center'>
+            HELPING TO MAKE SURE THIS IS ALL YOUR CONSUMER EVER THINKS ABOUT
+          </h2>
+
 					<p className='mb-4 text-white'>
 						For more information on how we can assist with your winemaking
 						needs, please visit our Services page. If you're ready to enhance
@@ -41,7 +22,7 @@ const LandingPage = () => {
 					</p>
 				</div>
 			</div>
-		</section>
+
 	);
 };
 
